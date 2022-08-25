854deaa59303b1a280f1ff3ca79daad93798e1bc
TJ <tjbecker.okc@gmail.com>
2022-08-24 20:27:40 -0500

    Applied styles to buttons on login/register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Log in') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -55,8 +55,8 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                <button type="submit" class="submit-button button-in-row btn btn-primary">
+                                    {{ __('Log in') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
@@ -70,7 +70,7 @@
                     <br>
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
-                            <a href="/register" class="btn btn-primary">
+                            <a href="/register" class="submit-button button-in-row btn btn-primary">
                                 New Coach
                             </a>
                         </div>
